b23fc5f15897a8799c2bdd9c7c85f172da81f6e8
Tumor micronenvironment Lab <157691334+ImmuneTME@users.noreply.github.com>
2025-11-24 09:37:29 +0100

    Refactor cell-cycle scores script with new paths
    Updated file paths and removed unnecessary comments. Adjusted filtering for output results.

--- 3.- Cell-cycle scores.R
+++ 3.- Cell-cycle scores.R
@@ -8,22 +8,19 @@ library(tidyverse)
 library(Seurat)
 library(reticulate)
 
-#load the giotto object with all (12 ChIO) samples preprocessed
-results_folder = "S:/VISIUM/basic_expression/plots/"
+#load the giotto object with all  samples preprocessed
+results_folder = "S://"
 
 py_config()
-my_python_path = 'C:/Users/angel/Documents/.virtualenvs/r-reticulate/Scripts/python.exe'
+my_python_path = 'C://python.exe'
 
 instrs = createGiottoInstructions(save_dir = results_folder,
                                   save_plot = TRUE,
                                   show_plot = TRUE,
                                   python_path = my_python_path)
 
-#directories to the preprocessed giotto objects
-#"S:/VISIUM/TFM/allsamples_giotto"
-#"S:/VISIUM/TFM/allsamples15_giotto"
 
-all_samples <- loadGiotto("S:/VISIUM/TFM/allsamples15_giotto",
+all_samples <- loadGiotto("C:/",
                           load_params = list(),
                           reconnect_giottoImage = TRUE,
                           python_path = my_python_path,
@@ -53,21 +50,9 @@ s_all_samples <- CellCycleScoring(s_all_samples,
 
 s_all_samples@meta.data -> s_allsamples_results
 #filter the output table. column indexes differ by giotto input
-#12 samples
-s_allsamples_results[, c(7,8, 10:16, 54:56)] -> filtered_sresults
-#15 samples
-s_allsamples_results[, c(7,8, 15, 19:21)] -> filtered_sresults
+s_allsamples_results[, c()] -> filtered_sresults
 
-#for the 12 samples with the 4 areas of interest:
-# Rename columns 4 to 50 by removing the first 4 characters
-colnames(filtered_sresults)[1:9] <- sub("^.{4}", "", colnames(filtered_sresults)[1:9])
-filtered_sresults <- filtered_sresults %>%
-  mutate(`area` = case_when(
-    mTLS == 1 ~ "mTLSs",
-    eTLS == 1 ~ "non-mTLSs",
-    noTLS_B == 1 ~ "Dispersed B cells",
-    `elses` == 1 ~ "Low B cell areas"
-  ))
 
-write.csv(filtered_sresults, "S:/VISIUM/cell-cycle/cycle_12samples.csv", row.names = TRUE, col.names = TRUE)
-write.csv(filtered_sresults, "S:/VISIUM/cell-cycle/cycle_15samples.csv", row.names = TRUE, col.names = TRUE)
+write.csv(filtered_sresults, "C:/.csv", row.names = TRUE, col.names = TRUE)
+
+
